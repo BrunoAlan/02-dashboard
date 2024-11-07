d03288c91fc2052b93aa97e89dbcad0a5943ff2b
BrunoAlan <alan.brunoab@gmail.com>
2024-11-07 08:18:32 -0300

    Redirect HomePage to the Counter dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function HomePage() {
-    return (
-        <>
-            <h1>Hello world</h1>
-        </>
-    );
+    redirect('/dashboard/counter');
 }
