b1afb41555e5c881d64349bcfc35864cb23ef86f
BrunoAlan <alan.brunoab@gmail.com>
2024-11-08 15:03:29 -0300

    Add PokemonGrid component to display a grid of Pokemon cards

---
+++ src/app/pokemons/components/PokemonGrid.tsx
@@ -0,0 +1,17 @@
+import { PokemonCard, SimplePlokemon } from '@/app/pokemons';
+import Image from 'next/image';
+
+interface Props {
+    pokemons: SimplePlokemon[];
+}
+export const PokemonGrid = ({ pokemons }: Props) => {
+    return (
+        <div className='flex flex-wrap gap-10 items-center justify-center'>
+            {pokemons.map((pokemon) => (
+                <div key={pokemon.id} className='flex flex-col items-center'>
+                    <PokemonCard key={pokemon.id} pokemon={pokemon} />
+                </div>
+            ))}
+        </div>
+    );
+};
