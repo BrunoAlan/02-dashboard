c9da733e2908141943356b4b12ad88f658d4ea08
BrunoAlan <alan.brunoab@gmail.com>
2024-11-08 14:32:35 -0300

    Fetch and display a list of Pokemons with images on the PokemonsPage

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,7 +1,41 @@
-export default function PokemonsPage() {
+import { PokemonResponse, SimplePlokemon } from '@/app/pokemons';
+import Image from 'next/image';
+
+const getPokemon = async ({
+    limit = 20,
+    offset = 0,
+}): Promise<SimplePlokemon[]> => {
+    const data: PokemonResponse = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+    ).then((response) => response.json());
+
+    const pokemons = data.results.map((pokemon) => ({
+        id: pokemon.url.split('/').at(-2)!,
+        name: pokemon.name,
+    }));
+
+    return pokemons;
+};
+
+export default async function PokemonsPage() {
+    const pokemons = await getPokemon({ limit: 100 });
     return (
-        <div>
-            <h1>Hello Page</h1>
+        <div className='flex flex-col'>
+            <div className='flex flex-wrap gap-10 items-center justify-center'>
+                {pokemons.map((pokemon) => (
+                    <div
+                        key={pokemon.id}
+                        className='flex flex-col items-center'
+                    >
+                        <Image
+                            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                            alt='Pokemon'
+                            width={100}
+                            height={100}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
