cbc649cfbb649a702816f00e91660b371119db0b
BrunoAlan <alan.brunoab@gmail.com>
2024-11-11 15:54:58 -0300

    Add Pokemon detail page and refactor PokemonCard component for improved routing

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 
 export const PokemonCard = ({ pokemon }: Props) => {
-    const { id, name } = pokemon;
+    const { name } = pokemon;
     return (
         <div className='mx-auto right-0 mt-2 w-60'>
             <div className='flex flex-col bg-white rounded overflow-hidden shadow-lg'>
@@ -27,7 +27,7 @@ export const PokemonCard = ({ pokemon }: Props) => {
                     <div className='mt-5'>
                         <Link
                             prefetch
-                            href={`pokemon/${id}`}
+                            href={`pokemons/${name}`}
                             className='border rounded-full py-2 px-4 text-xs font-semibold text-gray-100'
                         >
                             More
