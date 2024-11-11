df26b5dad00ae44d58620be9a4e6cb90baec9b96
BrunoAlan <alan.brunoab@gmail.com>
2024-11-11 20:56:03 -0300

    Add Favorites page and update Sidebar with new menu item

---
+++ src/app/dashboard/favorites/page.tsx
@@ -0,0 +1,18 @@
+import { PokemonGrid } from '@/pokemons';
+
+export const metadata = {
+    title: '151 Pokemon List',
+    description: 'List of 151 pokemons',
+};
+
+export default async function PokemonsPage() {
+    return (
+        <div className='flex flex-col'>
+            <span className='text-5xl my-2'>
+                Favorites pokemons{' '}
+                <small className='text-blue-500'>global state</small>
+            </span>
+            <PokemonGrid pokemons={[]} />
+        </div>
+    );
+}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,7 @@ import {
     IoBrowsersOutline,
     IoCalculator,
     IoFootball,
+    IoHeartCircleOutline,
     IoLogoReact,
 } from 'react-icons/io5';
 import { SidebarMenuItem } from './SidebarMenuItem';
@@ -25,6 +26,12 @@ const menuItems = [
         icon: <IoFootball size={40} />,
         title: 'Pokemons',
         subtitle: 'Static generation',
+    },
+    {
+        path: '/dashboard/favorites',
+        icon: <IoHeartCircleOutline size={40} />,
+        title: 'Favorites',
+        subtitle: 'Local storage',
     },
 ];
 
