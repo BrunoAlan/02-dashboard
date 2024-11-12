55079dee99329e7d4b70ff70d47553f1d16c813f
BrunoAlan <alan.brunoab@gmail.com>
2024-11-12 10:35:43 -0300

    Refactor CartCounter to use Redux for state management and dispatch actions

--- src/shoppint-cart/components/CartCounter.tsx
+++ src/shoppint-cart/components/CartCounter.tsx
@@ -1,23 +1,26 @@
 'use client';
-import { useState } from 'react';
+import { useAppDispatch, useAppSelector } from '@/store';
+import { addOne, subtractOne } from '@/store/counter/counterSlice';
 
 interface Props {
     value?: number;
 }
 export const CartCounter = ({ value = 0 }: Props) => {
-    const [count, setCount] = useState(value);
+    const count = useAppSelector((state) => state.counter.count);
+    const dispatch = useAppDispatch();
+
     return (
         <>
             <span className='text-9xl'>{count}</span>
             <div className='flex'>
                 <button
-                    onClick={() => setCount((prev) => prev + 1)}
+                    onClick={() => dispatch(addOne())}
                     className='flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2'
                 >
                     +1
                 </button>
                 <button
-                    onClick={() => setCount((prev) => prev - 1)}
+                    onClick={() => dispatch(subtractOne())}
                     className='flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2'
                 >
                     -1
