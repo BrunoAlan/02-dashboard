f414e3cf44d0e7801fa60d8e2f593324bc8d3339
BrunoAlan <alan.brunoab@gmail.com>
2024-11-13 09:54:08 -0300

    Export FavoritesPokemons component from index file

--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -1,6 +1,7 @@
 // start:ng42.barrel
 export { PokemonGrid } from './components/PokemonGrid';
 export { PokemonCard } from './components/PokemonCard';
+export { FavoritesPokemons } from './components/FavoritesPokemons';
 
 export type { PokemonResponse } from './interfaces/pokemons-response';
 export type { SimplePlokemon } from './interfaces/simple-pokemon';
