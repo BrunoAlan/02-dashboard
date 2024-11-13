d2ead0124ce74c1b0206d60d22e1681d33987b5f
BrunoAlan <alan.brunoab@gmail.com>
2024-11-13 10:42:45 -0300

    Add NoFavorites component to handle empty favorites state in FavoritesPokemons

--- src/pokemons/components/FavoritesPokemons.tsx
+++ src/pokemons/components/FavoritesPokemons.tsx
@@ -3,11 +3,27 @@
 import { useAppSelector } from '@/store';
 import { PokemonGrid } from './PokemonGrid';
 import { useState } from 'react';
+import { IoHeartOutline } from 'react-icons/io5';
 
 export const FavoritesPokemons = () => {
     const favoritePokemons = useAppSelector((state) =>
         Object.values(state.pokemons)
     );
     const [pokemons] = useState(favoritePokemons);
-    return <PokemonGrid pokemons={pokemons} />;
+    return (
+        <>
+            {pokemons.length ? (
+                <PokemonGrid pokemons={pokemons} />
+            ) : (
+                <NoFavorites />
+            )}
+        </>
+    );
 };
+
+export const NoFavorites = () => (
+    <div className='flex flex-col h-[50vh] items-center justify-center'>
+        <IoHeartOutline size={100} className='text-red-500' />
+        <span className='text-2xl text-gray-500'>No favorites Pokemons</span>
+    </div>
+);
